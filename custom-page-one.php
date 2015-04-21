5c223d4161a8b593dfdc926dbc5b611d0d0160d1
“Andrew” <arevisual@Gmail.com>
2015-04-21 17:45:18 -0400

    Installed theme Made headings red in style.css Made headdings center in style.css

---
+++ custom-page-one.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Template Name: Custom Page one
+*/
+get_header(); ?>
+    <div id="main2">
+        <div id="content-1"> <h2> Col 1 </h2> place to put your normal header stuff. It’s not actually part of the 3-column layout, but included here to make the layout a “complete” page.</div>
+        <div id="content-2">
+            <div id="content-2-1"><h2> Col 2 </h2> place to put your normal header stuff. It’s not actually part of the 3-column layout, but included here to make the layout a “complete” page.</div>
+            <div id="content-2-2"><h2> Col 3 </h2>place to put your normal header stuff. It’s not actually part of the 3-column layout, but included here to make the layout a “complete” page.</div>
+        </div>
+
+<?php get_footer(); ?>
